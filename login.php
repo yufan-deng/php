6e6e5bb02e1e4c45b28205f4edafcc5ccd46828b
Yufan Deng <yf-deng@outlook.com>
2022-07-24 17:56:27 +0800

    login_test1

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-Type: text/html; CHARSET=GBK");
+$comname=iconv('UTF-8','GBK',$_GET["comname"]);
 $username=iconv('UTF-8','GBK',$_GET["username"]);
 $password=$_GET["password"];
 $result="fail";
@@ -7,18 +8,15 @@ $result="fail";
 $servern="localhost";
 $coninfo=array("Database"=>"ysc","UID"=>"sa","PWD"=>"2002ivanDENG");
 $conn=sqlsrv_connect($servern,$coninfo);
-$sql="select [staffname],[password],rpt_42
-      from [ysc].[dbo].[userright]";
+$sql="Select companyid, custdefine, venderdefine, materdefine
+      from userright
+      where ostrc = '$comname' and staffname = '$username' and password = '$password'";
 $val=sqlsrv_query($conn,$sql);
-while($row=sqlsrv_fetch_array($val)){
-    if($row["staffname"]==$username){
-      if($row["password"]==$password){
-        $result="found";
-        if($row["rpt_42"]>1){
-          $result="success";
-        }
-      }
-    }
+if($row=sqlsrv_fetch_array($val)){
+  $result["companyid"] = $row["companyid"];
+  $result["custdefine"] = $row["custdefine"];
+  $result["venderdefine"] = $row["venderdefine"];
+  $result["materdefine"] = $row["materdefine"];
 }
 echo $result;
 sqlsrv_close($conn);
